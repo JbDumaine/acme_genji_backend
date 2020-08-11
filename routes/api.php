7f63dc07f859f8519e75d34a7685dc84ca4cad35
jbdumaine <jbdumaine@ecofinance.fr>
2020-08-11 18:32:11 +0200

    add backend register and login feature with passport and oauth2

--- routes/api.php
+++ routes/api.php
@@ -18,51 +18,64 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Suppliers routes
-Route::get('/suppliers', 'SupplierController@getAll');
-Route::get('/suppliers/{id}', 'SupplierController@get');
-Route::post('/suppliers', 'SupplierController@create');
-Route::put('/suppliers/{id}', 'SupplierController@update');
-Route::delete('/suppliers/{id}','SupplierController@destroy');
+Route::group([
+    'name' => 'secured.',
+    'middleware' => 'auth:api'
 
-//Products routes
-Route::get('/products', 'ProductController@getAll');
-Route::get('/products/{id}', 'ProductController@get');
-Route::post('/products', 'ProductController@create');
-Route::put('/products/{id}', 'ProductController@update');
-Route::delete('/products/{id}','ProductController@destroy');
+], function () {
+    //Suppliers routes
+    Route::get('/suppliers', 'SupplierController@getAll');
+    Route::get('/suppliers/{id}', 'SupplierController@get');
+    Route::post('/suppliers', 'SupplierController@create');
+    Route::put('/suppliers/{id}', 'SupplierController@update');
+    Route::delete('/suppliers/{id}', 'SupplierController@destroy');
 
-//Categories routes
-Route::get('/categories', 'CategoryController@getAll');
-Route::get('/categories/{id}', 'CategoryController@get');
-Route::post('/categories', 'CategoryController@create');
-Route::put('/categories/{id}', 'CategoryController@update');
-Route::delete('/categories/{id}','CategoryController@destroy');
+    //Products routes
+    Route::get('/products', 'ProductController@getAll');
+    Route::get('/products/{id}', 'ProductController@get');
+    Route::post('/products', 'ProductController@create');
+    Route::put('/products/{id}', 'ProductController@update');
+    Route::delete('/products/{id}', 'ProductController@destroy');
 
-//Stock Reception routes
-Route::get('/stock_receptions', 'StockReceptionController@getAll');
-Route::get('/stock_receptions/{id}', 'StockReceptionController@get');
-Route::post('/stock_receptions', 'StockReceptionController@create');
-Route::put('/stock_receptions/{id}', 'StockReceptionController@update');
-Route::delete('/stock_receptions/{id}','StockReceptionController@destroy');
+    //Categories routes
+    Route::get('/categories', 'CategoryController@getAll');
+    Route::get('/categories/{id}', 'CategoryController@get');
+    Route::post('/categories', 'CategoryController@create');
+    Route::put('/categories/{id}', 'CategoryController@update');
+    Route::delete('/categories/{id}', 'CategoryController@destroy');
 
-//Store routes
-Route::get('/stores', 'StoreController@getAll');
-Route::get('/stores/{id}', 'StoreController@get');
-Route::post('/stores', 'StoreController@create');
-Route::put('/stores/{id}', 'StoreController@update');
-Route::delete('/stores/{id}','StoreController@destroy');
+    //Stock Reception routes
+    Route::get('/stock_receptions', 'StockReceptionController@getAll');
+    Route::get('/stock_receptions/{id}', 'StockReceptionController@get');
+    Route::post('/stock_receptions', 'StockReceptionController@create');
+    Route::put('/stock_receptions/{id}', 'StockReceptionController@update');
+    Route::delete('/stock_receptions/{id}', 'StockReceptionController@destroy');
 
-//Product Stock Reception routes
-Route::get('/product_stock_receptions', 'ProductStockReceptionController@getAll');
-Route::get('/product_stock_receptions/{id}', 'ProductStockReceptionController@get');
-Route::post('/product_stock_receptions', 'ProductStockReceptionController@create');
-Route::put('/product_stock_receptions/{id}', 'ProductStockReceptionController@update');
-Route::delete('/product_stock_receptions/{id}','ProductStockReceptionController@destroy');
+    //Store routes
+    Route::get('/stores', 'StoreController@getAll');
+    Route::get('/stores/{id}', 'StoreController@get');
+    Route::post('/stores', 'StoreController@create');
+    Route::put('/stores/{id}', 'StoreController@update');
+    Route::delete('/stores/{id}', 'StoreController@destroy');
 
-//Command routes
-Route::get('/commands', 'CommandController@getAll');
-Route::get('/commands/{id}', 'CommandController@get');
-Route::post('/commands', 'CommandController@create');
-Route::put('/commands/{id}', 'CommandController@update');
-Route::delete('/commands/{id}','CommandController@destroy');
+    //Product Stock Reception routes
+    Route::get('/product_stock_receptions', 'ProductStockReceptionController@getAll');
+    Route::get('/product_stock_receptions/{id}', 'ProductStockReceptionController@get');
+    Route::post('/product_stock_receptions', 'ProductStockReceptionController@create');
+    Route::put('/product_stock_receptions/{id}', 'ProductStockReceptionController@update');
+    Route::delete('/product_stock_receptions/{id}', 'ProductStockReceptionController@destroy');
+
+    //Command routes
+    Route::get('/commands', 'CommandController@getAll');
+    Route::get('/commands/{id}', 'CommandController@get');
+    Route::post('/commands', 'CommandController@create');
+    Route::put('/commands/{id}', 'CommandController@update');
+    Route::delete('/commands/{id}', 'CommandController@destroy');
+});
+
+
+
+
+//Auth routes
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
