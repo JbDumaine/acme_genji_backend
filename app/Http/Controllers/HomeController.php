f89809cd9479f2b1af4f3e31c82e66639dcc32ec
rmn976 <rmn976dev@gmail.com>
2020-08-20 23:31:35 +0200

    Retirer le typage dans le HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,12 +13,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    private ProductRepository $productRepository;
-    private CommandRepository $commandRepository;
-    private StockReceptionRepository $receptionRepository;
-    private SupplierRepository $supplierRepository;
-    private CategoryRepository $categoryRepository;
-    private UserRepository $userRepository;
+    private $productRepository;
+    private $commandRepository;
+    private $receptionRepository;
+    private $supplierRepository;
+    private $categoryRepository;
+    private $userRepository;
 
     /**
      * Create a new controller instance.
